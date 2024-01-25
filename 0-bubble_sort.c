d9eb80a4dbd2ed4c2cf46da37b7b4b1da4ba4b56
walid-chihab <its.n.chihab@gmail.com>
2024-01-25 07:27:36 -0800

    -_-

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,41 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include "sort.h"
+
 /**
- * swap - swap function
- * @a: pointer to address of a
- * @b: pointer to address of b
- * Return: nothing
-*/
-void swap(int *a, int *b)
+ *swap - swaps two elements
+ *
+ *@xp: pointer to the first element
+ *@yp: pointer to the second element
+ */
+void swap(int *xp, int *yp)
 {
-	int temp = *a;
-	*a = *b;
-	*b = temp;
+int temp;
+
+temp = *xp;
+*xp = *yp;
+*yp = temp;
 }
 /**
- * bubble_sort - bubble sort algorithm
- * @array: array to be sorted
- * @size: size of the array
- * Return: nothing
-*/
+ *bubble_sort - function that sorts an array using bubble sort algorithms
+ *
+ * @array: array of integers to be sorted
+ *@size: number of elements in the array
+ */
 void bubble_sort(int *array, size_t size)
 {
-	size_t swapped, i, j;
+	size_t i, j;
 
-	if (array == NULL || size == 0)
+	if (array == NULL)
 		return;
-	for (i = 0; i < size; i++)
+
+	for (i = 0; i < size - 1; i++)
 	{
-		swapped = 0;
-		for (j = 0; j < size - 1; j++)
+		for (j = 0; j < size - i - 1; j++)
 		{
 			if (array[j] > array[j + 1])
 			{
 				swap(&array[j], &array[j + 1]);
-				swapped = 1;
 				print_array(array, size);
 			}
 		}
-		if (!swapped)
-			break;
+
 	}
 }
